b541b96bf3106cb8b2fd1a0d255b8cd7da5735d1
Marcel Šulek <marcel@sulek.eu>
2013-08-19 18:33:41 +0200

    grafika, zobrazeni poctu interpelaci

--- www/js/base.js
+++ www/js/base.js
@@ -1,10 +1,13 @@
 (function(){
-  var Poslanec, PoslanecList, this$ = this;
+  var list_barchart_height, list_barchart_bar_width, Poslanec, PoslanecList, this$ = this;
+  list_barchart_height = 50;
+  list_barchart_bar_width = 30;
   Poslanec = (function(){
     Poslanec.displayName = 'Poslanec';
     var prototype = Poslanec.prototype, constructor = Poslanec;
     function Poslanec(arg$){
       this.titul_pred = arg$.titul_pred, this.prijmeni = arg$.prijmeni, this.jmeno = arg$.jmeno, this.titul_za = arg$.titul_za, this.interpelace_source_count = arg$.interpelace_source_count, this.interpelace_target_count = arg$.interpelace_target_count, this.absence_count = arg$.absence_count, this.nazor_count = arg$.nazor_count, this.possible_votes_count = arg$.possible_votes_count;
+      this.interpelace_sum = this.interpelace_source_count + this.interpelace_target_count;
     }
     prototype.getName = function(){
       return this.titul_pred + " " + this.jmeno + " " + this.prijmeni + " " + this.titul_za;
@@ -19,17 +22,35 @@
       this.poslanci = poslanci;
       x$ = this.container = d3.select(parentSelector).append("ul");
       x$.attr('class', 'poslanecList');
-      this.container;
+      this.getScales();
       this.draw();
     }
     prototype.draw = function(){
-      var x$, y$;
-      x$ = this.container.data(this.poslanci).enter().append("li");
+      var x$, y$, z$, z1$, this$ = this;
+      x$ = this.container.selectAll("li").data(this.poslanci).enter().append("li");
       y$ = x$.append("span");
       y$.attr('class', 'name');
       y$.html(function(it){
         return it.getName();
       });
+      z$ = x$.append('div');
+      z$.attr('class', 'barchart');
+      z1$ = z$.append('div');
+      z1$.attr('class', "interpelace bar");
+      z1$.style('height', function(it){
+        return this$.interpelaceScale(it.interpelace_sum) + "px";
+      });
+      z1$.style('left', list_barchart_bar_width * 0);
+      return x$;
+    };
+    prototype.getScales = function(){
+      var interpelaceMaximum, x$;
+      interpelaceMaximum = Math.max.apply(Math, this.poslanci.map(function(it){
+        return it.interpelace_sum;
+      }));
+      x$ = this.interpelaceScale = d3.scale.linear();
+      x$.domain([0, interpelaceMaximum]);
+      x$.range([3, list_barchart_height]);
       return x$;
     };
     return PoslanecList;
--- www/ls/base.ls
+++ www/ls/base.ls
@@ -1,20 +1,41 @@
+list_barchart_height = 50
+list_barchart_bar_width = 30
+
 class Poslanec
     ({@titul_pred, @prijmeni, @jmeno, @titul_za, @interpelace_source_count, @interpelace_target_count, @absence_count, @nazor_count, @possible_votes_count}) ->
+        @interpelace_sum = @interpelace_source_count + @interpelace_target_count
+
     getName: -> "#{@titul_pred} #{@jmeno} #{@prijmeni} #{@titul_za}"
 
 class PoslanecList
     (parentSelector, @poslanci) ->
         @container = d3.select parentSelector .append "ul"
             ..attr \class \poslanecList
-        @container
+        @getScales!
         @draw!
+
     draw: ->
         @container
+            .selectAll "li"
             .data @poslanci
             .enter!append "li"
                 ..append "span"
                     ..attr \class \name
                     ..html (.getName!)
+                ..append \div
+                    ..attr \class \barchart
+                    ..append \div
+                        ..attr \class "interpelace bar"
+                        ..style \height ~>
+                            "#{@interpelaceScale it.interpelace_sum}px"
+                        ..style \left list_barchart_bar_width * 0
+
+    getScales: ->
+        interpelaceMaximum = Math.max ...@poslanci.map (.interpelace_sum)
+        @interpelaceScale = d3.scale.linear!
+            ..domain [0 interpelaceMaximum]
+            ..range [3 list_barchart_height]
+
 
 
 (err, data) <~ d3.json "./api.php?get=poslanci"
