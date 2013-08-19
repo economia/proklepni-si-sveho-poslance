d9f1ba0d6866d7b0e92e2e55d5b40573ee588d96
Marcel Šulek <marcel@sulek.eu>
2013-08-19 11:12:35 +0200

    zakladni router

--- www/api.php
+++ www/api.php
@@ -1,4 +1,14 @@
 <?php
 $config = require('./config.php');
+$allowed_methods = array('poslanci');
+$request = array();
+if(isset($_GET['get'])) {
+    $request = explode('/', $_GET['get']);
+}
+if(in_array($request[0], $allowed_methods)) {
+    $method = array_shift($request);
+    $method($request);
+}
+
 mysql_connect($config['db_host'], $config['db_user'], $config['db_pass']);
 mysql_select_db($config['db_name']);
