d37e16b60430de06ff874bb8d933d5a0c065315f
Marcel Šulek <marcel@sulek.eu>
2013-08-19 11:14:10 +0200

    router pro poslance

--- www/api.php
+++ www/api.php
@@ -9,6 +9,18 @@ if(in_array($request[0], $allowed_methods)) {
     $method = array_shift($request);
     $method($request);
 }
+function poslanci($params) {
+    if(!count($params)) {
+        get_poslanci_list();
+    } else {
+        get_poslanec(array_shift($params), $params);
+    }
+}
+function get_poslanci_list() {
 
+}
+function get_poslanec($id) {
+
+}
 mysql_connect($config['db_host'], $config['db_user'], $config['db_pass']);
 mysql_select_db($config['db_name']);
