a8562abfb7b89afcbae5de581c97eaed4c962556
Marcel Šulek <marcel@sulek.eu>
2013-08-19 19:06:19 +0200

    bar s absenci a vlastnimi nazory

--- www/js/base.js
+++ www/js/base.js
@@ -1,13 +1,14 @@
 (function(){
-  var list_barchart_height, list_barchart_bar_width, Poslanec, PoslanecList, this$ = this;
+  var list_barchart_height, Poslanec, PoslanecList, this$ = this;
   list_barchart_height = 50;
-  list_barchart_bar_width = 30;
   Poslanec = (function(){
     Poslanec.displayName = 'Poslanec';
     var prototype = Poslanec.prototype, constructor = Poslanec;
     function Poslanec(arg$){
       this.titul_pred = arg$.titul_pred, this.prijmeni = arg$.prijmeni, this.jmeno = arg$.jmeno, this.titul_za = arg$.titul_za, this.interpelace_source_count = arg$.interpelace_source_count, this.interpelace_target_count = arg$.interpelace_target_count, this.absence_count = arg$.absence_count, this.nazor_count = arg$.nazor_count, this.possible_votes_count = arg$.possible_votes_count, this.zakony_predkladatel_count = arg$.zakony_predkladatel_count;
       this.interpelace_sum = this.interpelace_source_count + this.interpelace_target_count;
+      this.absence_normalized = this.absence_count / this.possible_votes_count;
+      this.nazor_normalized = this.nazor_count / this.possible_votes_count;
     }
     prototype.getName = function(){
       return this.titul_pred + " " + this.jmeno + " " + this.prijmeni + " " + this.titul_za;
@@ -26,7 +27,7 @@
       this.draw();
     }
     prototype.draw = function(){
-      var x$, y$, z$, z1$, z2$, this$ = this;
+      var x$, y$, z$, z1$, z2$, z3$, z4$, this$ = this;
       x$ = this.container.selectAll("li").data(this.poslanci).enter().append("li");
       y$ = x$.append("span");
       y$.attr('class', 'name');
@@ -40,17 +41,25 @@
       z1$.style('height', function(it){
         return this$.interpelaceScale(it.interpelace_sum) + "px";
       });
-      z1$.style('left', list_barchart_bar_width * 0);
       z2$ = z$.append('div');
       z2$.attr('class', "zakony bar");
       z2$.style('height', function(it){
         return this$.zakonyScale(it.zakony_predkladatel_count) + "px";
       });
-      z2$.style('left', list_barchart_bar_width * 1);
+      z3$ = z$.append('div');
+      z3$.attr('class', "absence bar");
+      z3$.style('height', function(it){
+        return this$.percentageInvertedScale(it.absence_normalized) + "px";
+      });
+      z4$ = z$.append('div');
+      z4$.attr('class', "nazor bar");
+      z4$.style('height', function(it){
+        return this$.percentageScale(it.nazor_normalized) + "px";
+      });
       return x$;
     };
     prototype.getScales = function(){
-      var interpelaceMaximum, x$, zakonyMaximum, y$;
+      var interpelaceMaximum, x$, zakonyMaximum, y$, z$, z1$;
       interpelaceMaximum = Math.max.apply(Math, this.poslanci.map(function(it){
         return it.interpelace_sum;
       }));
@@ -63,7 +72,13 @@
       y$ = this.zakonyScale = d3.scale.linear();
       y$.domain([0, zakonyMaximum]);
       y$.range([3, list_barchart_height]);
-      return console.log(zakonyMaximum);
+      z$ = this.percentageScale = d3.scale.linear();
+      z$.domain([0, 1]);
+      z$.range([3, list_barchart_height]);
+      z1$ = this.percentageInvertedScale = d3.scale.linear();
+      z1$.domain([1, 0]);
+      z1$.range([3, list_barchart_height]);
+      return z1$;
     };
     return PoslanecList;
   }());
--- www/ls/base.ls
+++ www/ls/base.ls
@@ -1,8 +1,9 @@
 list_barchart_height = 50
-
 class Poslanec
     ({@titul_pred, @prijmeni, @jmeno, @titul_za, @interpelace_source_count, @interpelace_target_count, @absence_count, @nazor_count, @possible_votes_count, @zakony_predkladatel_count}) ->
-        @interpelace_sum = @interpelace_source_count + @interpelace_target_count
+        @interpelace_sum    = @interpelace_source_count + @interpelace_target_count
+        @absence_normalized = @absence_count / @possible_votes_count
+        @nazor_normalized   = @nazor_count / @possible_votes_count
 
     getName: -> "#{@titul_pred} #{@jmeno} #{@prijmeni} #{@titul_za}"
 
@@ -31,6 +32,14 @@ class PoslanecList
                         ..attr \class "zakony bar"
                         ..style \height ~>
                             "#{@zakonyScale it.zakony_predkladatel_count}px"
+                    ..append \div
+                        ..attr \class "absence bar"
+                        ..style \height ~>
+                            "#{@percentageInvertedScale it.absence_normalized}px"
+                    ..append \div
+                        ..attr \class "nazor bar"
+                        ..style \height ~>
+                            "#{@percentageScale it.nazor_normalized}px"
 
 
     getScales: ->
@@ -43,8 +52,14 @@ class PoslanecList
         @zakonyScale = d3.scale.linear!
             ..domain [0 zakonyMaximum]
             ..range [3 list_barchart_height]
-        console.log zakonyMaximum
 
+        @percentageScale = d3.scale.linear!
+            ..domain [0 1]
+            ..range [3 list_barchart_height]
+
+        @percentageInvertedScale = d3.scale.linear!
+            ..domain [1 0]
+            ..range [3 list_barchart_height]
 
 
 (err, data) <~ d3.json "./api.php?get=poslanci"
