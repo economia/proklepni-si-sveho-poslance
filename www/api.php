8799c91f899fbe2caac80dbdfa30ca8fa17231cb
Marcel Šulek <marcel@sulek.eu>
2013-08-19 11:21:04 +0200

    zobrazeni vsech poslancu v db

--- www/api.php
+++ www/api.php
@@ -2,25 +2,34 @@
 $config = require('./config.php');
 $allowed_methods = array('poslanci');
 $request = array();
+mysql_connect($config['db_host'], $config['db_user'], $config['db_pass']);
+mysql_select_db($config['db_name']);
+mysql_query("SET CHARACTER SET utf8");
+
 if(isset($_GET['get'])) {
     $request = explode('/', $_GET['get']);
 }
 if(in_array($request[0], $allowed_methods)) {
     $method = array_shift($request);
-    $method($request);
+    $result = $method($request);
+    header('Content/type: application/json');
+    echo json_encode($result);
 }
 function poslanci($params) {
     if(!count($params)) {
-        get_poslanci_list();
+        return get_poslanci_list();
     } else {
-        get_poslanec(array_shift($params), $params);
+        return get_poslanec(array_shift($params), $params);
     }
 }
 function get_poslanci_list() {
-
+    $result = mysql_query("SELECT * FROM poslanci");
+    $r = array();
+    while ($row = mysql_fetch_assoc($result)) {
+        $r[] = $row;
+    }
+    return $r;
 }
 function get_poslanec($id) {
 
 }
-mysql_connect($config['db_host'], $config['db_user'], $config['db_pass']);
-mysql_select_db($config['db_name']);
