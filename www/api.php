03181fe2f1ac1b8804ed2ada41520b6562845ffc
Marcel Šulek <marcel@sulek.eu>
2013-08-23 13:11:27 +0200

    informace, zda je poslanec nove zvoleny - API

--- www/api.php
+++ www/api.php
@@ -52,6 +52,7 @@ function get_poslanci_list() {
     while ($row = mysql_fetch_assoc($result)) {
         $row['poslanec_2010']             = (bool)$row['poslanec_2010'];
         $row['preferencni']               = (bool)$row['preferencni'];
+        $row['novacek']                   = (bool)$row['novacek'];
         $row['id']                        = (int)$row['id'];
         $row['interpelace_target_count']  = (int)$row['interpelace_target_count'];
         $row['interpelace_source_count']  = (int)$row['interpelace_source_count'];
